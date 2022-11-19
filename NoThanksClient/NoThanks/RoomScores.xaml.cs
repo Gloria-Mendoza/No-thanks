579ba48dda177e45e4624328f4e808ec5517fa4a
Panther-14 <lego.ninjago.city@gmail.com>
2022-11-19 08:57:43 -0600

    Actualizaciones menores

---
+++ NoThanksClient/NoThanks/RoomScores.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Shapes;
+
+namespace NoThanks
+{
+    /// <summary>
+    /// Lógica de interacción para RoomScores.xaml
+    /// </summary>
+    public partial class RoomScores : Window
+    {
+        private List<PlayerManager.Player> playersList = new List<PlayerManager.Player>();
+
+        public RoomScores()
+        {
+            InitializeComponent();
+            GenerateScores(playersList);
+        }
+
+        public void GenerateScores(List<PlayerManager.Player> players)
+        {
+            players.ForEach(p => CardsScore(p));
+            lxtScores.ItemsSource = players;
+        }
+
+        public void CardsScore(PlayerManager.Player player)
+        {
+            var score = 0;
+            for (int i = 0; i < playersList.Count; i++)
+            {
+                score += (int)player.Cards[i];
+            }
+            player.TotalScore = score;
+            playersList.Add(player);
+        }
+    }
+}
